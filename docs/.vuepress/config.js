1cbf132c804b9618354076aa9d962eb22ed6ff73
xinpingwang <xinping.wang@hotmail.com>
2022-03-31 09:29:01 +0800

    feat: config nav links

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,9 @@
 module.exports = {
   title: "Open Flex",
+  themeConfig: {
+    nav: [
+      { text: 'JavaScript 函数式编程指南', link: 'https://open-flex.github.io/mostly-adequate-guide-chinese/' },
+      { text: 'Java 编程思想', link: 'https://open-flex.github.io/thinking-in-java-zh/' },
+    ]
+  }
 };
